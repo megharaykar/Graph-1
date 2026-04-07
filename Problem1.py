620ea82286fb8e710c5c35732468ef7ec0b0814f
Megha Raykar <meghamanjunathraykar@Meghas-MacBook-Air.local>
2026-04-07 16:26:50 -0700

    Comments added

--- Problem1.py
+++ Problem1.py
@@ -3,8 +3,9 @@
 # Graph problem
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
-# TC: O(E) + O(V)
-# SC: O(V)
+# TC: O(E) + O(V) => We need to first go over the entire input array of tuples (edges) to construct the indegrees
+# then we need to traverse the entire indegrees array of size V
+# SC: O(V) => indegrees array of size V
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
